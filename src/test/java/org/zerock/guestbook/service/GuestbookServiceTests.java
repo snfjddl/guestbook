c15ea485e0d14db8fce68eac617a29f0573b9359
snfjddl <seokil1220@gmail.com>
2022-01-11 18:19:15 +0900

    Service 계층과 DTO 클래스 구현

---
+++ src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
@@ -0,0 +1,47 @@
+package org.zerock.guestbook.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
+import org.zerock.guestbook.entity.Guestbook;
+
+@SpringBootTest
+public class GuestbookServiceTests {
+
+    @Autowired
+    private GuestbookService guestbookService;
+
+    @Test
+    public void registOneEntity() {
+
+        GuestbookDTO guestbookDTO = GuestbookDTO.builder()
+                .title("Test Title....")
+                .content("Test Content...")
+                .writer("user0")
+                .build();
+
+        System.out.println(guestbookService.register(guestbookDTO));
+
+    }
+
+    @Test
+    public void isChangeEntityToDto() {
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> pageResultDTO = guestbookService.getList(pageRequestDTO);
+
+        System.out.println("PREV: " + pageResultDTO.isPrev());
+        System.out.println("NEXT: " + pageResultDTO.isNext());
+        System.out.println("TOTAL: " + pageResultDTO.getTotalPage());
+        System.out.println("--------------------------------------------------");
+        for (GuestbookDTO dto : pageResultDTO.getDtoList()) {
+            System.out.println(dto);
+        }
+        System.out.println("--------------------------------------------------");
+        pageResultDTO.getPageList().forEach(i -> System.out.println(i));
+    }
+}
