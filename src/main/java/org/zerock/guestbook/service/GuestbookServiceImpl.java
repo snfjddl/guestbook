a31ec9aeb92d32f35bf4b2e9c54117501917827d
snfjddl <seokil1220@gmail.com>
2022-01-12 11:02:39 +0900

    방명록의 조회 처리

--- src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -13,6 +13,7 @@ import org.zerock.guestbook.dto.PageResultDTO;
 import org.zerock.guestbook.entity.Guestbook;
 import org.zerock.guestbook.repository.GuestbookRepository;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 @Service
@@ -35,6 +36,14 @@ public class GuestbookServiceImpl implements GuestbookService {
         repository.save(entity);
 
         return entity.getGno();
+    }
+
+    @Override
+    public GuestbookDTO read(Long gno) {
+
+        Optional<Guestbook> result = repository.findById(gno);
+
+        return result.isPresent() ? entityToDto(result.get()) : null;
     }
 
     @Override
